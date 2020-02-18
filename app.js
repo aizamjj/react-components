847f6ae9254584537ba694f634587ff9b9b5e7d6
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-02-18 14:46:05 -0800

    Write class for Grocery List

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// TODO
+var App = () => (
+    <div>
+      <h2>My Grocery List</h2>
+      <GroceryList groceries={['Cucumber', 'Kale', 'Tomato']}/> 
+    </div>
+  );
+
+const app = document.getElementById("app");
+ReactDOM.render(<GroceryList />, app);
+
+class GroceryListItem  extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hover:false,
+        };
+    }
+    //make bold when mouse hover
+    toggleHover() {
+        this.setState({
+          hover: !this.state.hover
+        });
+      }
+    render() {
+        var groceries = ['Cucumber, Kale, Tomato']
+        return (
+        <li>{this.props.groceries}</li>        
+        );
+    }
+
+}
+
+var GroceryList = (props) => (
+    <div>
+        {props.groceries.map(groceryItem =>
+        <GroceryListItem groceryItem={groceryItem} />
+        )}
+    </div>
+
+);
+
+
+ 
